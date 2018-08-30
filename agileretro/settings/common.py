b5740c0b3d63338fbb9cd637f48ae4dd0d1a4259
silentServer <brian.hao211@gmail.com>
2018-08-30 14:47:05 +0800

    Refine database connection

--- agileretro/settings/common.py
+++ agileretro/settings/common.py
@@ -131,3 +131,14 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly'
     ]
 }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PWD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+    }
+}
--- agileretro/settings/development.py
+++ agileretro/settings/development.py
@@ -6,16 +6,5 @@ DEBUG = True
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'agileretro',
-        'USER': 'root',
-        'PASSWORD': 'toor',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
-}
-
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 

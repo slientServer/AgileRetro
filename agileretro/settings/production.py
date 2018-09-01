b6614c4861ea32b3ceccf990b7280d3f78663e70
silentServer <brian.hao211@gmail.com>
2018-09-01 17:16:50 +0800

    Add email support for production

--- agileretro/settings/production.py
+++ agileretro/settings/production.py
@@ -3,3 +3,13 @@ from decouple import config
 
 DEBUG = False
 ALLOWED_HOSTS = config('ALLOWED_HOSTS')
+
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = config('EMAIL_HOST', default='')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=True, cast=bool)
+
+DEFAULT_FROM_EMAIL = 'Agile_Center <agile_center@sohu.com>'
+EMAIL_SUBJECT_PREFIX = '[Agile Retro] '
